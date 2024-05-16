4eeddace6f0f8db2595d34c99c460b890cd4f953
dami <devdami0828@gmail.com>
2024-05-16 20:05:24 +0900

    fix router

--- router-basic/frontend/static/js/index.js
+++ router-basic/frontend/static/js/index.js
@@ -4,36 +4,30 @@ import Posts from "../views/Posts.js";
 import PostDetail from "../views/PostDetail.js";
 import MyPage from "../views/MyPage.js";
 
+// :id 처럼 path로 오는 파라미터를 정규표현식으로 만든다.
+// 이를 통해 동적 라우팅에서 매번 변화되는 path 파라미터 부분을 분리하기 위함
 const pathToRegex = (path) =>
   new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
-
-const getRouteParams = (match) => {
-  const values = match.result.slice(1);
-  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
-    (result) => result[1]
-  );
-
-  return Object.fromEntries(
-    keys.map((key, i) => {
-      return [key, values[i]];
-    })
-  );
-};
 
 const updateUrlRoute = (url) => {
   history.pushState(null, null, url);
   router();
 };
 
+// url 일치하는 라우터를 새로운 배열로 만들어서 반환
 const findMatchingRoute = (routes) => {
   const match = routes.map((route) => {
     return {
       route: route,
+      // url이 정규식과 일치하는 전체 문자열을 첫 번째 요소로 포함하는 Array를 반환
+      // [ "/posts/2", "2" ] 이 담긴다.
       result: location.pathname.match(pathToRegex(route.path)),
     };
   });
   findRoute(match, routes);
 };
+
+// router에 담긴 배열들 중에서 일치하는 result만 찾아서 해당하는 view를 그려주기 위한 로직
 const findRoute = (match, routes) => {
   let matchParams = match.find((params) => params.result !== null);
   if (!matchParams) {
@@ -44,6 +38,7 @@ const findRoute = (match, routes) => {
   }
   renderView(matchParams);
 };
+
 const renderView = (match) => {
   const view = match.route.view();
   document.querySelector("#app").innerHTML = view;
@@ -63,7 +58,7 @@ const router = () => {
 // popstate은 사용자가 브라우저의 뒤로 가기, 앞으로 가기 버튼 클릭했을때만 발생하는 이벤트
 window.addEventListener("popstate", router);
 
-const linkButtonClick = () => {
+const handleLinkClicks = () => {
   document.body.addEventListener("click", (e) => {
     const matches = e.target.matches("[data-link]");
     if (!matches) return;
@@ -73,6 +68,6 @@ const linkButtonClick = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  linkButtonClick();
+  handleLinkClicks();
   router();
 });
